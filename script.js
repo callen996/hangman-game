b6ba2d1d8bf250ba4949c7fd41f499cb02c51d75
Jakesmitty05 <jakesmitty05@gmail.com>
2023-11-09 21:15:04 -0500

    added var for number of blanks

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var randomWordUrl = "https://random-word-api.vercel.app/api?words=1&length=7&type=uppercase"
 var sadGifUrl = "https://api.giphy.com/v1/gifs/random?api_key=VZP80HfNoEAn2JpnCtbMfqEGN4HAuZF7&tag=sad&rating=pg-13"
+var guessThisWord
+var sadGif
+var blanksNum
+
 
 fetch(randomWordUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    alert(data)
+    guessThisWord = data[0]
+    console.log(guessThisWord)
+    blanksNum = guessThisWord.length
+    console.log(blanksNum)
   });
 
   fetch(sadGifUrl)
@@ -14,6 +21,7 @@ fetch(randomWordUrl)
     return response.json();
   })
   .then(function (data) {
-    alert(data.data.url)
+    sadGif = data.data.url
+    console.log(sadGif)
   });
 
