6c62902de0fab18ad69926f9b688c6ff64833c9d
Jakesmitty05 <jakesmitty05@gmail.com>
2023-11-09 19:09:58 -0500

    Gets random word from api

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+var randomWordUrl = "https://random-word-api.vercel.app/api?words=1&length=7&type=uppercase"
 
+fetch(randomWordUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    alert(data)
+  });
