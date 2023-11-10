1a17938fa489d7a2eb3c6c7fe59b5266ed037feb
Jakesmitty05 <jakesmitty05@gmail.com>
2023-11-09 20:31:16 -0500

    added gif api

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var randomWordUrl = "https://random-word-api.vercel.app/api?words=1&length=7&type=uppercase"
+var sadGifUrl = "https://api.giphy.com/v1/gifs/random?api_key=VZP80HfNoEAn2JpnCtbMfqEGN4HAuZF7&tag=sad&rating=pg-13"
 
 fetch(randomWordUrl)
   .then(function (response) {
@@ -6,4 +7,12 @@ fetch(randomWordUrl)
   })
   .then(function (data) {
     alert(data)
+  });
+
+  fetch(sadGifUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    alert(data.data.url)
   });
